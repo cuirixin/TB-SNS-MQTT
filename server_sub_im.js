32ca63a09896db79738b2f6053bbe391add1a32e
Victor <rixincui@gmail.com>
2015-12-05 22:54:34 +0800

    UP

--- server_sub_im.js
+++ server_sub_im.js
@@ -1,22 +1,34 @@
-"""
-离线消息，
-"""
 
 var mqtt = require('mqtt');
 var config = require('./config')
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+	name: 'server_pub',
+	streams: [
+		{
+		  level: 'info',
+		  path: './logs/im_info.log'
+		  //stream: process.stdout            // log INFO and above to stdout 
+		},
+		{
+		  level: 'error',
+		  path: './logs/im_error.log'  // log ERROR and above to a file 
+		}
+	]
+});
 
 // 不需要离线消息，cleanSession设置为true
 var client  = mqtt.connect(config.mqtt_url, {clean:true}); // test.mosquitto.org
 
 client.on('connect', function () {
-  console.log("Connect Success.");
+  log.info("Connect Success.");
   client.subscribe('im/+',{qos:1});
 });
 
 client.on('message', function (topic, message) {
   // message is Buffer
   uid = topic.split("/")[1]
-  console.log("Server publish : " + 'noti/'+uid + " - " + message.toString());
-  client.publish('noti/'+uid, {"code": global.Sys.cont.NEW_USER_MESSAGE} ,{qos:0});
+  log.info("Server publish : " + 'noti/'+uid);
+  client.publish('noti/'+uid, {"code": global.Sys.cont.CODE_USER_IM} ,{qos:0});
 });
 
